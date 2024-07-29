5a872b489564fbe4699dafc98e92f73bc9b8d23d
damia <damia@ECLIPSE>
2024-07-29 09:53:08 +0200

    Aggiungere i file di progetto.

---
+++ TeslaMTF/Class1.cs
@@ -0,0 +1,30 @@
+﻿using Exiled.API.Features;
+using Exiled.Events;
+using System;
+using TeslaMTF;
+
+
+namespace Tesla
+{
+    public class TeslaPlugin : Plugin<Config>
+    {
+        public override string Name => "TeslaMTF";
+        public override string Author => "Vacca";
+        public override string Prefix => "TeslaMTF";
+        public override Version RequiredExiledVersion => new Version(8,9,11);
+
+        private EventHandler _eventHandler;
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.TriggeringTesla += TeslaHandler.OnTriggeringTesla;
+            base.OnEnabled();
+            Log.Info("Plugin Attivo");
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.TriggeringTesla -= TeslaHandler.OnTriggeringTesla;
+            base.OnDisabled();
+        }
+    }
+}
---
+++ TeslaMTF/EventHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles;
+
+
+namespace TeslaMTF
+{
+    public class TeslaHandler
+    {
+        public static List<TeslaGate> DisabledTeslaGates { get; private set; } = new List<TeslaGate>();
+
+        public static void OnTriggeringTesla(TriggeringTeslaEventArgs ev)
+        {
+
+            if (ev.Player.IsNTF)
+            {
+                ev.IsAllowed = false;
+
+                ev.IsTriggerable = false;
+                Log.Info("Metodo chiamato");
+            }
+        }
+
+
+
+
+    }
+}
